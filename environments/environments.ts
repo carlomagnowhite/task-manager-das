5cfcff7ed7d3bf79ff3eb74cb1b526194ce4dcb6
Matt <mateo19dg@gmail.com>
2024-12-09 18:21:01 -0500

    Se envia servicio completo de supabase.

---
+++ environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://wredbncjlcohrhtejpbh.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyZWRibmNqbGNvaHJodGVqcGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3NzI1MzMsImV4cCI6MjA0OTM0ODUzM30.CXs43U8H1vHfswgBHwsjV3yfjjb4GAs5FAiKayXBQP8',
+}
